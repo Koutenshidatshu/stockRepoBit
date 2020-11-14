b99c4b9370126d528c16017384f00a3e94a7b2ff
yonathan <koutenshidatshu_555@yahoo.co.id>
2020-11-15 02:03:07 +0700

    create view model test and integration

---
+++ stockBit/View Model/NewsViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  NewsViewModel.swift
+//  stockBit
+//
+//  Created by Yonathan Wijaya on 15/11/20.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class NewsViewModel {
+    private let provider: NewsProvider
+    lazy var news: Observable<[Article]> = newsRelay.asObservable()
+    
+    private var newsRelay = BehaviorRelay<[Article]>(value: [])
+    private let disposeBag = DisposeBag()
+    
+    init(provider: NewsProvider) {
+        self.provider = provider
+    }
+    
+    func getNews() {
+        provider.get()
+            .subscribe(onNext: { [weak self] result in
+                self?.newsRelay.accept(result.data)
+            }).disposed(by: disposeBag)
+    }
+    
+    func filterByCategory(name: String) {
+        let filterNews = newsRelay.value.filter { $0.categories.lowercased().contains(name.lowercased())}
+        newsRelay.accept(filterNews)
+    }
+    
+    func itemAt(index: Int) -> Article {
+        return newsRelay.value[index]
+    }
+    
+    func newsCount() -> Int {
+        return newsRelay.value.count
+    }
+    
+}
---
+++ stockBitTests/UnitTest/NewsViewModelTests.swift
@@ -0,0 +1,80 @@
+//
+//  NewsViewModelTests.swift
+//  stockBitTests
+//
+//  Created by Yonathan Wijaya on 15/11/20.
+//
+
+import XCTest
+import RxSwift
+@testable import stockBit
+
+class NewsViewModelTests: XCTestCase {
+    
+    private var viewModel: NewsViewModel!
+    private var providerMock : NewsProvider!
+    private let disposeBag = DisposeBag()
+    
+    override func setUp() {
+        super.setUp()
+        providerMock = NewsProviderMock()
+        
+        viewModel = NewsViewModel(provider: providerMock)
+        
+    }
+    
+    func testGetNews() {
+        var result = [Article]()
+        
+        viewModel.news
+            .subscribe(onNext: { res in
+                result = res
+            }).disposed(by: disposeBag)
+        
+        viewModel.getNews()
+        
+        XCTAssertNotNil(result)
+        XCTAssertGreaterThan(result.count, 1)
+        XCTAssertEqual(viewModel.newsCount(), 197)
+        
+    }
+    
+    func testFilterNewsBasedOnCategory() {
+        
+        viewModel.news
+            .subscribe(onNext: { _ in
+            }).disposed(by: disposeBag)
+        
+        viewModel.getNews()
+        viewModel.filterByCategory(name: "btc")
+        
+        XCTAssertEqual(viewModel.newsCount(), 7)
+    }
+    
+}
+
+
+class NewsProviderMock: NewsProvider {
+    var sample = NewsApiMock().sample
+    private var newsList: NewsResponse?
+    
+    func get() -> Observable<NewsResponse> {
+        return .just(decodeToResponse(from: sample)!)
+    }
+    
+    private func decodeToResponse(from jsonString: String) -> NewsResponse? {
+        do {
+            let data = Data(sample.utf8)
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(NewsResponse.self, from: data)
+        } catch {
+            return nil
+        }
+        
+    }
+    
+    func emit(news: NewsResponse) {
+        newsList = news
+    }
+}
